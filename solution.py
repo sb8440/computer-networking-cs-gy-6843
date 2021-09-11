e42f4ac151910b9a5abcc8f1d33edcdd302949a2
sb8440 <sb8440@nyu.edu>
2021-09-11 16:54:15 -0400

    fix md5

--- solution.py
+++ solution.py
@@ -22,7 +22,7 @@ def welcome_assignment_answers(question):
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
        answer = hashlib.md5(b'NYU Computer Networking').hexdigest()
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
